a707151fbb31f724f354312c10f9670f7d47ba0f
Carroll <m.carroll@northeastern.edu>
2026-01-26 15:13:50 -0500

    upsert job for all poles

--- fetch-poles.ts
+++ fetch-poles.ts
@@ -1,4 +1,5 @@
-//import { prisma } from './lib/prisma'
+import { prisma } from './lib/prisma'
+import type {UtilityPoleUpdateInput, UtilityPoleWhereUniqueInput} from "./generated/prisma/models/UtilityPole";
 
 const NEWTON_GIS_ENDPOINT = 'https://gisweb.newtonma.gov/server/rest/services/Data/MapServer/1/query?f=json&where=1%3D1&outFields=OBJECTID,NUM,STREET,MAP&outSR=4326&f=json&geometryType=esriGeometryEnvelope&geometry=%7B%22spatialReference%22%3A%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D%2C%22xmin%22%3A-7928164.97820771%2C%22ymin%22%3A5208264.452124099%2C%22xmax%22%3A-7925947.504086458%2C%22ymax%22%3A5209671.073047367%2C%22type%22%3A%22esriGeometryEnvelope%22%7D';
 
@@ -28,29 +29,29 @@ async function fetchPoles() {
          }
     });
 }
-/*
-TODO is this even necessary now that I have imported the data once? It's unlikely poles will be updated/added/removed.
- Keeping this file for posterity.
 
+// TODO this only creates/updates poles. What if one is removed? I don't want to delete the record from the DB because
+//  part of this project is to archive poles before they are removed.
 async function main() {
     const poles = await fetchPoles();
 
-    const queries = poles.map( ( pole ) => {
-        return prisma.utilityPole.update({
-            where: {
-                id: pole.id,
-            },
-            data: {
-                streetName: pole.streetName,
-                streetNum: pole.streetNum,
-                latitude: pole.latitude,
-                longitude: pole.longitude,
-            },
-        });
-    })
-
     try {
-        const transaction = await prisma.$transaction(queries);
+        const transaction = await prisma.$transaction(
+            async () => poles.map( ( pole ) => {
+                return prisma.utilityPole.upsert({
+                    where: {
+                        id: pole.id,
+                    },
+                    create: pole,
+                    update: {
+                        streetName: pole.streetName,
+                        streetNum: pole.streetNum,
+                        latitude: pole.latitude,
+                        longitude: pole.longitude,
+                    },
+                });
+            }),
+        );
         console.log(`Updated ${transaction.length} records`);
     } catch (error) {
         console.error('Transaction failed:', error);
@@ -66,4 +67,3 @@ main()
         await prisma.$disconnect()
         process.exit(1)
     })
- */
