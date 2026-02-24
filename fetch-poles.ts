3bce61547347a04a3e207dd09bbf2ed12734eb17
Marin @Home <marin@marincarroll.com>
2026-02-23 20:41:21 -0500

    typescript fixes from prisma-examples

--- fetch-poles.ts
+++ fetch-poles.ts
@@ -1,5 +1,4 @@
 import { prisma } from './lib/prisma'
-import type {UtilityPoleUpdateInput, UtilityPoleWhereUniqueInput} from "./generated/prisma/models/UtilityPole";
 
 const NEWTON_GIS_ENDPOINT = 'https://gisweb.newtonma.gov/server/rest/services/Data/MapServer/1/query?f=json&where=1%3D1&outFields=OBJECTID,NUM,STREET,MAP&outSR=4326&f=json&geometryType=esriGeometryEnvelope&geometry=%7B%22spatialReference%22%3A%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D%2C%22xmin%22%3A-7928164.97820771%2C%22ymin%22%3A5208264.452124099%2C%22xmax%22%3A-7925947.504086458%2C%22ymax%22%3A5209671.073047367%2C%22type%22%3A%22esriGeometryEnvelope%22%7D';
 
